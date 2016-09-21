0abcf35da485b9173caab231ee9b8ff5b5cb65ff
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-21 13:05:09 +0100

    chore(coveralls-coverage): modify karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -83,18 +83,9 @@ module.exports = function(config) {
     // how many browser should be started simultaneous
     concurrency: Infinity
   });
-<<<<<<< 07eee20cd51a03e59274a31aa821d47200a189ba
-=======
 
   if (process.env.TRAVIS) {
     config.browsers = ['Chrome_travis_ci'];
   }
-};
->>>>>>> chore(coveralls-coverage): modify karma.conf
-
-  if (process.env.TRAVIS) {
-      configuration.browsers = ['Chrome_travis_ci'];
-  }
-
 };
 
