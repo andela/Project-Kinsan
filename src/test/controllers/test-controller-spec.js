59a7abb51659123375a20ecd3075393ca0d4ff00
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-26 16:45:18 +0100

    chore(coveralls-coverage): fix code indenting

--- src/test/controllers/test-controller-spec.js
+++ src/test/controllers/test-controller-spec.js
@@ -4,24 +4,24 @@ describe('TestController', function() {
     controller;
 
     beforeEach(function() {
-      module("test");
+      module('test');
 
       inject(function($injector) {
-        $rootScope = $injector.get("$rootScope");
+        $rootScope = $injector.get('$rootScope');
         $scope = $rootScope.$new();
-        $scope.controller = $injector.get("$controller")("TestController",
+        $scope.controller = $injector.get('$controller')('TestController',
             {$scope: $scope});
         });
     });
 
-    describe("Initial time", function() {
-      it("has the value of 200", function() {
+    describe('Initial time', function() {
+      it('has the value of 200', function() {
         expect($scope.time).toEqual(200);
       });
     });
 
-    describe("Count Down function works", function() {
-      it("reduces the time by ten", function() {
+    describe('Count Down function works', function() {
+      it('reduces the time by ten', function() {
         $scope.countDown();
         expect($scope.time).toEqual(190);
       });
