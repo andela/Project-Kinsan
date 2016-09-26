2cfcb75a5aa9df6514ad428ec5d2f633fb7a6329
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-26 16:49:12 +0100

    chore(coveralls-coverage): fix code indenting

--- src/app/controllers/test-controller.js
+++ src/app/controllers/test-controller.js
@@ -1,10 +1,10 @@
-angular.module("test", []).controller("TestController",
-  ["$scope", function($scope) {
-      $scope.time = 200;
-      $scope.countDown = function() {
-          if($scope.time) {
-              $scope.time -= 10;
-          }
-      };
+angular.module('test', []).controller('TestController',
+  ['$scope', function($scope) {
+    $scope.time = 200;
+    $scope.countDown = function() {
+      if($scope.time) {
+        $scope.time -= 10;
+      }
+    };
   }]
 );
