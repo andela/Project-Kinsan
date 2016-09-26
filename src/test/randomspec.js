30e5c761fab885dae41a81e8b12cd7901213f0da
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-26 16:40:04 +0100

    chore(coveralls-coverage): fix code indenting

--- src/test/randomspec.js
+++ src/test/randomspec.js
@@ -1,5 +1,5 @@
 describe("Base test", function() {
-    it("should pass", function() {
-        expect(true).toBe(true);
-    });
+  it("should pass", function() {
+    expect(true).toBe(true);
+  });
 });
