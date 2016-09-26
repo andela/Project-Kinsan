a100b0e5e0039ec4b64a0e7e30fedd3ee2d802e0
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-09-26 17:18:08 +0100

    chore(coveralls-coverage): fix code indenting

--- src/test/controllers/test-controller-spec.js
+++ src/test/controllers/test-controller-spec.js
@@ -3,27 +3,27 @@ describe('TestController', function() {
     $scope,
     controller;
 
-    beforeEach(function() {
-      module('test');
+  beforeEach(function() {
+    module('test');
 
-      inject(function($injector) {
-        $rootScope = $injector.get('$rootScope');
-        $scope = $rootScope.$new();
-        $scope.controller = $injector.get('$controller')('TestController',
-          {$scope: $scope});
-        });
-    });
-
-    describe('Initial time', function() {
-      it('has the value of 200', function() {
-        expect($scope.time).toEqual(200);
+    inject(function($injector) {
+      $rootScope = $injector.get('$rootScope');
+      $scope = $rootScope.$new();
+      $scope.controller = $injector.get('$controller')('TestController',
+        {$scope: $scope});
       });
-    });
+  });
 
-    describe('Count Down function works', function() {
-      it('reduces the time by ten', function() {
-        $scope.countDown();
-        expect($scope.time).toEqual(190);
-      });
+  describe('Initial time', function() {
+    it('has the value of 200', function() {
+      expect($scope.time).toEqual(200);
     });
+  });
+
+  describe('Count Down function works', function() {
+    it('reduces the time by ten', function() {
+      $scope.countDown();
+      expect($scope.time).toEqual(190);
+    });
+  });
 });
