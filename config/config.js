d3c49457b963c392facb2de93902798ce2ea9f7e
bolorundurowb <ogatimo@gmail.com>
2016-09-27 07:06:57 +0100

    chore(modularity): moves the server secret to the env file

--- config/config.js
+++ config/config.js
@@ -5,6 +5,3 @@ var _ = require('underscore');
 module.exports = _.extend(
     require(__dirname + '/../config/env/all.js'),
     require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
-
-// Server variable for generating JWT
-module.exports.secret = 'b3e739d25115ad6c137fa990076857e39300b6d19deef477a9c4c6bce799fce1';
