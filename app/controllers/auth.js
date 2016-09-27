52000b67c86828b7db3e20290caa76139592a3b0
bolorundurowb <ogatimo@gmail.com>
2016-09-27 05:06:40 +0100

    feature(authentication): generates and returns a JWT on successful login.

---
+++ app/controllers/auth.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const config = require('./../../config/config');
+
+// Load user model
+let User = mongoose.model('User');
+
+let auth = {
+  // Function for verifying a users regiatration and
+  // generating a JWT on successful login.
+  login: function(req, res) {
+    User.findOne({
+      username: req.body.username
+    }, function(err, user) {
+      if(err) {
+        res.send(err);
+      }
+      if (!user) {
+        res.json({
+          success: false,
+          message: 'Authentication failed. User not found.'
+        });
+      } else if (user) {
+        if (user.password !== req.body.password) {
+          res.json({
+            success: false,
+            message: 'Authentication failed. Wrong password.'
+          });
+        } else {
+          let token = jwt.sign(user, config.secret, {
+            expiresIn: '24h'
+          });
+
+          res.json({
+            success: true,
+            message: 'Authentication successful.',
+            token: token
+          });
+        }
+      }
+    });
+  }
+};
+
+module.exports = auth;
