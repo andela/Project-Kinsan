260bfd6a5b88a7e5e16bfa3463332704bfee2a40
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-09-28 20:12:40 +0100

    Feature(history-test): Add test files for angular history code
    Add spec files to test the angular js code to be written for
    getting authenticated user's game history. The test files will
    contain tests for an angular factory(service) and angular
    controller.

---
+++ test/app/history.test.js
@@ -0,0 +1,7 @@
+// TODO: add a test to test the controller
+
+// TODO: add a test for the service
+
+// TODO: add a test that tests that the service returns a valid array of object
+
+// TODO: add a test that tests that the service returns a message when the resource doesn't exist
