97fc149ec57c0670da019ada8cafe84bb7877a46
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-09-29 06:28:49 +0100

    Feature(service-test): Add spec to test History service is defined
    Add some specs to the history.test.js file to test if
    the History service is defined. This is just a basic
    test to ascertain the existence of the existence of the
    service.

--- test/app/history.test.js
+++ test/app/history.test.js
@@ -5,3 +5,23 @@
 // TODO: add a test that tests that the service returns a valid array of object
 
 // TODO: add a test that tests that the service returns a message when the resource doesn't exist
+
+describe('History factory', function() {
+  let History;
+
+  // before each test load the angular module
+  beforeEach(
+    angular.mock.module('mean')
+  );
+
+  // Set our injected History service (_Users_) to our local History variable
+  beforeEach(inject(function(_History_){
+    History = _History_;
+  }));
+
+  // A simple test to verify the History service exists
+  it('should exist', function() {
+    expect(History).toBeDefined();
+  });
+
+});
