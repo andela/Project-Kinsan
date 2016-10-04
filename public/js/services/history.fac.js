38680e3f6b021421f1ad9ad478cc30a0d6e516a9
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-10-04 13:00:08 +0100

    Feature(Implement-getHistory): Implement getHistory() of HistoryFac
    implement getHistory() function in HistoryFac service to make a
    http request to /api/users/:id/history to get user history

--- public/js/services/history.fac.js
+++ public/js/services/history.fac.js
@@ -2,10 +2,15 @@
 
   'use strict';
 
-  angular.module('services.History', [])
-        .factory('HistoryFac', function(){
+  angular.module('services.History')
+        .factory('HistoryFac', function($http){
           return {
-
+            getHistory: function(userId) {
+              return $http.get('/api/users/'+userId+'/history')
+                          .then(function(res) {
+                            return res.data;
+                          });
+            }
           };
         });
 })();
