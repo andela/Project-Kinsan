e2ffbe3063be61b835fa09e177859c544fc1ebd2
Chukwuma Ezumezu <chukwuma.ezumezu@andela.com>
2016-10-10 15:55:58 +0100

    chore(env-variable) fix hound report

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 var path = require('path'),
-    rootPath = path.normalize(__dirname + '/../..');
+  rootPath = path.normalize(__dirname + '/../..');
 
 module.exports = {
-    root: rootPath,
-    port: process.env.PORT || 3000,
-    db: process.env.MONGODB_URI,
-    secret: process.env.SECRET
+  root: rootPath,
+  port: process.env.PORT || 3000,
+  db: process.env.MONGODB_URI,
+  secret: process.env.SECRET
 };
