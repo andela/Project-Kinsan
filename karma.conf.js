09d2037f4f9e04a5e9cb58ac1d560600825699b1
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-10-17 14:22:42 +0100

    Feature(add-ng-html2js-to-karma): Add ng-html2js preprocessor to karma

--- karma.conf.js
+++ karma.conf.js
@@ -28,9 +28,11 @@ module.exports = function(config) {
       'public/lib/firebase/firebase.js',
       'public/js/socket.io.js',
       'public/lib/hello/dist/hello.all.min.js',
+      'public/js/*.js',
       'public/js/**/*.js',
       'public/js/controllers/*.js',
       'public/js/services/*.js',
+      'public/views/*.tpl.html', // Ralph: load template file for the directives
       'test/app/*.test.js',
       'test/app/**/*.test.js',
       {pattern: 'test/app/response.json', watched: true, served: true, included: false},
@@ -49,7 +51,12 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/app/**/*.js': ['coverage']
+      './src/app/**/*.js': ['coverage'],
+      'public/views/*.tpl.html': 'ng-html2js' // Ralph: add ng-html2js to watch the template files
+    },
+
+    ngHtml2JsPreprocessor: {
+      stripPrefix: 'public/' //Ralph: strip public away from the link of the template
     },
 
     coverageReporter: {
