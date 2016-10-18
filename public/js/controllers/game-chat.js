fbfe095f044f03da7776020d50082fe8753127e9
Ologho Cyril Paul <cyril.paul@andela.com>
2016-10-18 12:34:25 +0100

    feature(chat): in game
    Refactor game-chat controller and integrate it to frontend
    Refactor gamechat factory, add isTyping functionality
    Complete writing tests for game-chat controller and game-chat factory
    
    Start #130341363

--- public/js/controllers/game-chat.js
+++ public/js/controllers/game-chat.js
@@ -4,37 +4,122 @@
 'use strict';
 angular.module('mean.gameChat')
   .controller('GameChatCtrl', function ($scope, GameChat) {
-    var gameId = 'rkhu3yj4';
-    var users = [{name:'john_doe', avatar:'http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_male_dark_on_clear_200x200.png'}, {name:'jane_doe',avatar:'http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_female_dark_on_clear_200x200.png'}];
 
-    $scope.hi = 'Hello';
+      var gameId = 'rkhu3yj4',
+          users = [{name:'john_doe', avatar:'http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_male_dark_on_clear_200x200.png'}, {name:'jane_doe',avatar:'http://brandonmathis.com/projects/fancy-avatars/demo/images/avatar_female_dark_on_clear_200x200.png'}],
+          currentUser = users[0];
+
+      // var gameId = game.gameID,
+      //   users = game.players;
+      
     $scope.GameChat = GameChat;
     $scope.messages = {};
 
-    var newMessage = function(messages) {
+    $scope.newMessage = function(messages) {
       $scope.messages = messages;
-      $scope.$apply();
+      //$scope.$apply();
     };
 
-    $scope.sendMessage = function(msg, sender){
-      sender = users[1];
-      GameChat.sendMessage(msg, gameId, sender);
+    $scope.sendMessage = function() {
+      $scope.GameChat.sendMessage($scope.message, gameId, currentUser);
     };
 
+    //triggerIsTyping : function(gameId, typist){
+    $scope.showIsTyping = function(){
+      $scope.GameChat.triggerIsTyping(gameId, currentUser);
+    };
+
+    $scope.hideIsTyping = function(){
+      var ele = document.getElementById('isTyping');
+      ele.display = 'none';
+    };
+
+    $scope.typingListener = function(typist){
+      var ele = document.getElementById('isTyping');
+       // ele.innerHTML =  'someone is typing....';
+    };
+
+    /*
+    *Checks if a game chat session already exists
+    */
     GameChat.sessionExists(gameId)
     .then(function() {
-      GameChat.loadMessages(gameId)
-      .then(function(messages){
+      // Loads message from that section
+      $scope.GameChat.loadMessages(gameId)
+      .then(function(messages) {
         $scope.messages = messages;
         $scope.$apply();
-        GameChat.listenForMessage(gameId, newMessage);
+        console.log($scope.messages);
+        //Listen for new incoming message
+        $scope.GameChat.listenForMessage(gameId, function(messages){
+          $scope.newMessage(messages);
+        });
+
+        //Listen for user typing
+        $scope.GameChat.listenForTyping(gameId,function (typist) {
+            $scope.typingListener(typist);
+        });
       });
     })
     .catch(function(){
-      GameChat.startSession(gameId, users).then(function(){
-        //now listen for messages
-        GameChat.listenForMessage(gameId, newMessage);
+      //No game chat session existed before
+      $scope.GameChat.startSession(gameId, users).then(function(){
+      //now listen for messages
+      $scope.GameChat.listenForMessage(gameId, function(){
+        $scope.newMessage(messages);
       });
-    });
 
+      //Listen for user typing
+      $scope.GameChat.listenForTyping(gameId, $scope.typingListener(function (typist) {
+          $scope.typingListener(typist)
+      }));
+    })
   });
+
+  /*Manipulate the DOM */
+  $(document).ready( function () {
+  var scrolled = 100000;
+
+  $('#chat').click( function () {
+    $('#word').slideToggle('slow');
+    $('#box').slideToggle('slow');
+    $('#chat').toggleClass('closeChat');
+    $('#chat').toggleClass('chat');
+    $('#chatMessages').animate({
+      scrollTop: scrolled
+    });
+  });
+
+  $('#top').click( function () {
+    $('#word').slideToggle('slow');
+    $('#box').slideToggle('slow');
+    $('#chat').toggleClass('closeChat');
+    $('#chat').toggleClass('chat');
+  });
+
+/*  $('#message').focusin( function () {
+    $('#isTyping').text('I am typing....');
+    $('#message').focusout( function () {
+      $('#isTyping').text('');
+    });
+  });*/
+
+  $('#send').click( function () {
+
+    $('#message').val('');
+    $('#chatMessages').animate({
+      scrollTop: scrolled
+    });
+  });
+
+  $('#message').keypress( function (e) {
+    if(e.which == 13) {
+      $('#message').val('');
+      $('#chatMessages').animate({
+        scrollTop: scrolled
+      });
+    }
+  });
+});
+      
+});
--- test/app/game-chat.test.js
+++ test/app/game-chat.test.js
@@ -1,19 +1,241 @@
 'use strict';
 describe('Game chat test', function(){
+
   beforeEach(
     angular.mock.module('mean.gameChat')
   );
 
-  describe('Test for game chat service', function(){
-    // Load HistoryFac, $httpBackend(mocks $http service) and $q service
-    beforeEach(inject(function(_HistoryFac_, _$httpBackend_, _$q_){
-      HistoryFac = _HistoryFac_;
-      $httpBackend = _$httpBackend_;
-      $q = _$q_;
-    }));
-    it('should expect GameChat to be defined', function(){
+  var $controller, $factoryProvider, GameChat;
 
-    })
+  beforeEach(function(){
+    inject(function(_$controller_){
+      $controller = _$controller_;
+    });
+
+    inject(function($injector){
+      $factoryProvider = $injector;
+    });
+    GameChat = $factoryProvider.get('GameChat');
+  });
+
+  describe('Test GameChat factory', function(){
+    //creating demo data
+    var validGameId = 'qwertyuiop',
+      users = [
+        {name:'John Doe', avatar:'http://blahblah.com/avatar.png'},
+        {name:'Jane Doe',avatar:'http://blahblah.com/gravatar.gif'}
+      ];
+
+    beforeEach(function(){
+      /*
+      * we try to change the key for saving data to firebase after
+      * confirming they've been defined
+      * */
+      if (typeof GameChat.sessionStoreRef === 'string'
+         && GameChat.sessionStoreRef.trim() !== '' ) {
+        GameChat.sessionStoreRef = 'test-game-sessions';
+      }
+
+      if (typeof GameChat.membersRef === 'string'
+        && GameChat.membersRef.trim() !== '') {
+        GameChat.membersRef = 'test-members';
+      }
+
+      if (typeof GameChat.messagesRef === 'string'
+        && GameChat.messagesRef.trim() !== '') {
+        GameChat.messagesRef = 'test-messages';
+      }
+
+      if(typeof GameChat.isTypingRef === 'string' 
+        && GameChat.isTypingRef.trim() !== '') {
+        GameChat.isTypingRef = 'test-isTyping';
+      }
+
+    });
+
+    describe('Check for properties', function () {
+      it('should expect property "sessionStoreRef" to be defined',function () {
+        expect(GameChat.sessionStoreRef).toBeDefined();
+      });
+
+      it('should expect property "messagesRef" to be defined', function(){
+        expect(GameChat.messagesRef).toBeDefined();
+      });
+
+      it('should expect property "membersRef" to be defined', function(){
+        expect(GameChat.membersRef).toBeDefined();
+      });
+    });
+
+    describe('Game session', function(){
+      it('should expect "startSession" to be a defined function', function(){
+        expect(typeof GameChat.startSession).toEqual('function');
+      });
+
+      it('should expect "sessionExists" to be a defined function', function(){
+        expect(typeof GameChat.sessionExists).toEqual('function');
+      });
+
+      it('should be able to create a game session', function(done){
+        GameChat.startSession(validGameId, users).then(function(data){
+          GameChat.sessionExists(validGameId).then(function(data){
+            expect(data).toBe(true);
+            done();
+          }).catch(function(){
+            expect(false).toBe(true);
+            done();
+          });
+        });
+      });
+
+
+      it('should assert that a game session with a gameId exist',function(done){
+        var nonExistentGameId = 'asdf1234';
+        GameChat.sessionExists(nonExistentGameId).then(function(data){
+          expect(data).toBe(false);
+          GameChat.sessionExists(validGameId).then(function(data){
+            expect(data).toBe(true);
+            done();
+          }).catch(function(){
+            expect(false).toBe(true);
+            done();
+          })
+        });
+      });
+    });
+
+    describe('Messages', function(){
+
+      it('should expect sendMessage to be a defined function', function(){
+        expect(GameChat.sendMessage).toBeDefined();
+        expect(typeof GameChat.sendMessage).toEqual('function');
+      });
+
+      it('should be able to send a message', function(done){
+        GameChat.sendMessage('Hello world', validGameId, users[0])
+          .then(function(data){
+            expect(data).toBe(true);
+            done();
+          })
+          .catch(function(error){
+            expect(false).toBe(true);
+            done();
+          });
+      });
+
+      it('should expect listenForMessage to be a defined function', function(){
+        expect(GameChat.listenForMessage).toBeDefined();
+        expect(typeof GameChat.listenForMessage).toEqual('function');
+      });
+      
+      it('should detect new data changes to firebase', function(done){
+        var listener = {
+          listen : function () {
+            console.log('New message detected!');
+          }
+        };
+        spyOn(listener, 'listen');
+        GameChat.listenForMessage(validGameId, listener.listen);
+        GameChat.sendMessage('Hey', validGameId, users[1]).then(function () {
+          expect(listener.listen).toHaveBeenCalled();
+          done();
+        }).catch(function () {
+          expect(false).toBe(true);
+          done();
+        });
+      });
+
+      it('should expect loadMessages to be a defined function', function(){
+        expect(GameChat.loadMessages).toBeDefined();
+        expect(typeof GameChat.loadMessages).toEqual('function');
+      });
+
+      it('should expect loadMessages to retrieve all messages', function(done){
+        var sentMessages = ['Hello world', 'Hey'];
+        GameChat.loadMessages(validGameId).then(function(data){
+          expect(Object.keys(data).length).toBe(sentMessages.length);
+         for(var i in data){
+           if(data.hasOwnProperty(i)){
+            expect(sentMessages.indexOf(data[i].message))
+            .toBeGreaterThanOrEqual(0);
+           }
+         }
+        done();
+        }).catch(function(){
+          expect(false).toBe(true);
+          done();
+        });
+      });
+    });
+    describe('Typing watcher', function(){
+      var listener = {
+        isTyping : function(){
+          console.log('You are typing');
+        }
+      };
+      it('should expect triggerIsTyping to be a defined function', function(){
+        expect(typeof GameChat.triggerIsTyping).toEqual('function');
+      });
+
+      it('should expect listenForTyping to be a defined function', function(){
+        expect(typeof GameChat.listenForTyping).toEqual('function');
+      });
+
+      it('should expect listener.isTyping to be called', function(done){
+        spyOn(listener, 'isTyping');
+        GameChat.listenForTyping(validGameId, listener.isTyping);
+        GameChat.triggerIsTyping(validGameId, {name : users[0].name})
+        .then(function(){
+          expect(listener.isTyping).toHaveBeenCalled();
+          done();
+        }).catch(function(){
+          expect(false).toBe(true);
+          done();
+        })
+
+      });
+      //
+    });
+
+  });
+
+  describe('Test for GameChatCtrl', function(){
+
+    var $scope, controller, service;
+    beforeEach(function(){
+      $scope = {};
+      controller = $controller('GameChatCtrl', {$scope: $scope});
+      service = $factoryProvider.get('GameChat');
+      console.log('Yo',service);
+      $scope.GameChat.sessionStoreRef = 'test-game-sessions'
+    });
+
+    describe('Scope properties check', function(){
+      it('should expect property "GameChat" to be defined', function(){
+        expect($scope.GameChat).toEqual(GameChat);
+      });
+
+      it('should expect property "messages" to be an empty object', function(){
+        expect($scope.messages).toEqual({});
+      });
+
+      it('should expect newMessage method to be a defined function', function(){
+        expect(typeof $scope.newMessage).toEqual('function');
+      });
+
+      it('should expect sendMessage method to be a defined function', function(){
+        expect(typeof $scope.sendMessage).toEqual('function');
+      });
+
+    });
+
+
+    it('should expect newMessage to receive an object as parameter',function () {
+      //expect( typeof $scope.newMessage.calls.argsFor(0)[0] ).toEqual('object');
+    });
+
+
+
   });
 });
 
