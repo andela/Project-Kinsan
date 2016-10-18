bce40e2b2a0765ce037fb28ec0bd757658ad35ab
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-10-18 17:03:30 +0100

    Feature(chat-component): Add a directive for the chat feature

--- public/js/directives.js
+++ public/js/directives.js
@@ -46,8 +46,7 @@ angular.module('mean.directives', [])
       restrict: 'E',
       templateUrl: '/views/chat.tpl.html',
       controllerAs: 'vm',
-      controller: {
-        
+      controller: function(){
       }
     };
   });
