afc41cc8cad2ea0b0acdfd9bcd5169fa1ad66b38
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-10-21 06:40:27 +0100

    Release(initialisation): Add the variables needed for the game component

--- ng-2/app/components/game.component.ts
+++ ng-2/app/components/game.component.ts
@@ -2,10 +2,22 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params }   from '@angular/router';
 import { Location }                 from '@angular/common';
 
+import { GameService }              from './../services/game.service';
+
 @Component({
     selector: 'game-screen',
-    templateUrl: './app/templates/game-screen.components.html'
+    templateUrl: './app/templates/game-screen.components.html',
+    styleUrls: ['./app/style/game.css']
 })
 export class GameComponent {
-
+    constructor(
+        private location: Location,
+        private gameService: GameService
+    ){}
+    hasPickedCards: boolean = false;
+    winningCardPicked: boolean = false;
+    showTable: boolean = false;
+    modalShown: boolean = false;
+    game = gameService.getGame(); //from game service
+    pickedCards: any = [];
 }
