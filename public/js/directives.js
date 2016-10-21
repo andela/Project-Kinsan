bf54185cf295b39a8487a938cabb0bfb0d1e75bf
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-10-21 15:10:01 +0100

    Feature(chat-component): Converted the chat feature into a service

--- public/js/directives.js
+++ public/js/directives.js
@@ -1,5 +1,5 @@
-angular.module('mean.directives', [])
-  // Ralph: Modify player directive players on the game screen
+angular.module('mean.directives')
+  // Ralph: Modify player directive
   .directive('player', function (){
     return{
       restrict: 'E',
@@ -56,10 +56,11 @@ angular.module('mean.directives', [])
     return {
       restrict: 'E',
       templateUrl: '/views/chat.tpl.html',
-      // eslint-disable-next-line
+      scope: {
+      },
+      bindToController: true,
       controllerAs: 'vm',
-      controller: function(){
-      }
+      controller: chatController
     };
   }).directive('infoModal', function(){
     return {
@@ -71,3 +72,112 @@ angular.module('mean.directives', [])
       }
     };
   });
+
+function chatController ($scope, GameChat, game, Global) {
+  $scope.currentUser = Global.user || game.players[game.playerIndex];
+  $scope.gameId = game.gameID;
+  $scope.users = game.players;
+
+  $scope.GameChat = GameChat;
+  $scope.messages = {};
+
+  $scope.newMessage = function(messages) {
+    $scope.messages = messages;
+    $scope.$apply();
+  };
+
+  $scope.sendMessage = function() {
+    return $scope.GameChat.sendMessage($scope.message, $scope.gameId, $scope.currentUser);
+  };
+
+  // triggerIsTyping : function($scope.gameId, typist){
+  $scope.showIsTyping = function(){
+    //we need to make the user unique for same user so the listener is triggered
+    $scope.currentUser.rand = Math.ceil(Math.random() * 10000);
+    var typist = {
+      name: $scope.currentUser.username || $scope.currentUser.name,
+      rand: $scope.currentUser.rand
+    };
+    $scope.GameChat.triggerIsTyping($scope.gameId, typist);
+  };
+
+  var firstCheck = false, waitTime = 2, timer;
+  $scope.typingListener = function(typist){
+
+    if (firstCheck) {
+      waitTime = 2;
+      clearInterval(timer);
+      if ($scope.currentUser.name !== typist.name) {
+        $('#isTyping').html(typist.name + ' is typing...');
+      }
+
+      timer = setInterval(function() {
+        waitTime--;
+        if(waitTime === 0){
+          $('#isTyping').html('');
+          clearInterval(timer);
+        }
+      }, 1000);
+
+    }
+    firstCheck = true;
+  };
+
+  /*
+  *Checks if a game chat session already exists
+  */
+  $scope.GameChat.sessionExists($scope.gameId)
+  .then(function() {
+    // Loads message from that section
+    $scope.GameChat.loadMessages($scope.gameId)
+    .then(function(messages) {
+      $scope.messages = messages;
+      $scope.$apply();
+      //Listen for new incoming message
+      $scope.GameChat.listenForMessage($scope.gameId, function(messages){
+        $scope.newMessage(messages);
+      });
+
+      //Listen for user typing
+      $scope.GameChat.listenForTyping($scope.gameId, $scope.typingListener);
+    });
+  })
+  .catch(function(){
+    //No game chat session existed before
+    $scope.GameChat.startSession($scope.gameId, $scope.users).then(function(){
+      //now listen for messages
+      $scope.GameChat.listenForMessage($scope.gameId, function(messages){
+        $scope.newMessage(messages);
+      });
+
+      //Listen for user typing
+      $scope.GameChat.listenForTyping($scope.gameId, $scope.typingListener);
+    });
+  });
+
+  $(document).ready( function () {
+    var scrolled = 100000, messageInput = $('#message');
+    messageInput.keyup(function(){
+      $scope.showIsTyping();
+    });
+
+    $('#send').click( function () {
+      $('#message').val('');
+      $('#chatMessages').animate({
+        scrollTop: scrolled
+      });
+    });
+
+    messageInput.keypress(function (e) {
+      if(e.which === 13) {
+        $scope.sendMessage();
+        $('#message').val('');
+        $('#chatMessages').animate({
+          scrollTop: scrolled
+        });
+      }
+    });
+
+  });
+
+}
